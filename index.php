620001079b97b62dc7bc04fa5697b78441937c25
Greg Cedarblade <gmcedarblade@gmail.com>
2017-04-06 15:25:22 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        $filePointer = fopen("printers.txt", "r");
+        
+        $ips = array();
+        $printerType - array();
+        $buildings = array();
+        $roomName = array();
+        
+        $printersInBuilding = array();
+        
+        while(feof($filePointer)) {
+            
+            
+            
+        }
+        
+        ?>
+    </body>
+</html>
